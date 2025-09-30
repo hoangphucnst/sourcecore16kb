7221a61a835f8bcd50a0bf17ccd5f5d6b66a18f0
Nguyễn Quốc An <annq@nst.com.vn>
2025-09-30 11:30:34 +0700

    [AnDev] * Update bootsplash config iOS

--- ios/myapp/AppDelegate.swift
+++ ios/myapp/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import RNBootSplash
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,10 +40,15 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   }
 
   override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
+    #if DEBUG
+        RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+        Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+    #endif
+  }
+
+   override func customize(_ rootView: RCTRootView) {
+    super.customize(rootView)
+    RNBootSplash.initWithStoryboard("BootSplash", rootView: rootView)
   }
 }
